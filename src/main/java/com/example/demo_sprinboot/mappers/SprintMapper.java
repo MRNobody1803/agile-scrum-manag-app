2aae0d2df83382bc3bdb2a3c7b38fdc7972eb125
Abdes <abdessamadelhachami@gmail.com>
2025-03-18 00:46:22 +0000

    fonctionality SprintBacklog : Finshed

--- src/main/java/com/example/demo_sprinboot/mappers/SprintMapper.java
+++ src/main/java/com/example/demo_sprinboot/mappers/SprintMapper.java
@@ -3,9 +3,11 @@ package com.example.demo_sprinboot.mappers;
 import com.example.demo_sprinboot.DTO.SprintDTO;
 import com.example.demo_sprinboot.entities.Sprint;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface SprintMapper {
+    SprintMapper INSTANCE = Mappers.getMapper(SprintMapper.class);
     SprintDTO toDto(Sprint sprint);
     Sprint toEntity(SprintDTO sprintDTO);
 }
---
+++ src/main/java/com/example/demo_sprinboot/repository/SprintBacklogRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo_sprinboot.repository;
+
+import com.example.demo_sprinboot.entities.SprintBacklog;
+import com.example.demo_sprinboot.entities.Status;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SprintBacklogRepository extends JpaRepository<SprintBacklog, Long> {
+    List<SprintBacklog> findBySprintId(Long sprintId);
+    @Modifying
+    @Transactional
+    @Query("UPDATE SprintBacklog sb SET sb.name = :newName WHERE sb.id = :id")
+    void updateSprintBacklogName(@Param("id") Long id, @Param("newName") String newName);
+
+    // Trouver les SprintBacklogs contenant un certain mot-clé dans leur nom
+    @Query("SELECT sb FROM SprintBacklog sb WHERE LOWER(sb.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<SprintBacklog> searchByNom(@Param("keyword") String keyword);
+
+}
